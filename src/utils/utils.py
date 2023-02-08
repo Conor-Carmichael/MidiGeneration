a2ef31529e1439ead0d87d29beb9e018883a1e5b
Conor-Carmichael <conorbcarmichael@gmail.com>
2023-02-08 12:27:12 -0500

    util functions for note conversions, accompanying tests

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,4 +1,27 @@
 from typing import *
+from numpy import random
+from math import pow
+import numpy as np
+from src.theory.notes import Note
+
+
+def calc_semitone_diff_pitches(a:float, b:float) -> int:
+    """Returns absolute value, no direction"""
+    diff = 12.00 * np.log2(a / b)
+   
+    return np.absolute(round(diff))
+
+
+def calc_semitone_diff_notes(a:Note, b:Note) -> int:
+    return calc_semitone_diff_pitches(a.pitch, b.pitch)
+
+
+def get_pitch_from_midi_value(midi) -> int:
+    exp = midi - 69.00
+    exp = exp / 12.00
+    fact = np.power(2, exp)
+    pitch = 440.00 * fact
+    return round(pitch, 2)
 
 
 def cycle_n_times(input: List, n: int) -> List:
@@ -26,3 +49,11 @@ def parse_roman_numeral_to_chord_type(input: str):
     # Check for chord alterations:
 
     return None
+
+
+def get_rand_velocities(mean: int, var: float, min: int, max: int):
+    v = random.normal(mean, var)
+    v = round(v, 0)
+    v = 0 if v < min else v
+    v = 127 if v > max else v
+    return v
