c225d422e1bf06b3563dd93e868a1e8df24c15e6
Conor-Carmichael <conorbcarmichael@gmail.com>
2023-02-11 14:20:27 -0500

    Added imports

--- src/theory/__init__.py
+++ src/theory/__init__.py
@@ -2,3 +2,8 @@ from typing import *
 from src.theory.constants import *
 from src.theory.datatypes import *
 import midiutil as midi
+
+from src.theory.notes import Note, NoteGeneric
+from src.theory.note_sequence import NoteSequence, NotesFactory
+from src.theory.scales import Scale, ScaleFactory, AllScaleFactories
+from src.theory.chords import Chord
