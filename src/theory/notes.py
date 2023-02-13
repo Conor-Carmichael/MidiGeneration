86c69b92dca3a5df79e916722960025f0a75316a
Conor-Carmichael <conorbcarmichael@gmail.com>
2023-02-13 14:49:45 -0500

    Documentation

--- src/theory/notes.py
+++ src/theory/notes.py
@@ -11,7 +11,7 @@ import numpy as np
 
 class NoteGeneric:
 
-    """Note class with no pitch or midi information"""
+    """Note class with no pitch or midi information. Basically just the name."""
 
     def __init__(self, name: str, next_note=None, prev_note=None) -> None:
         self.name = name
@@ -34,7 +34,10 @@ class NoteGeneric:
         return self.next_note
 
     def sharpen(self, keep_base_note_name: bool):
-        """Does not change base note name if not requested, just adds sharp"""
+        """
+        Does not change base note name if not requested, just adds sharp.
+        Sometimes it is more proper to have a Bbb rather than A. 
+        """
         if keep_base_note_name:
             self.name += SHARP
             self.alter += SHARP
@@ -44,6 +47,10 @@ class NoteGeneric:
             self.alter = self.name[1:] if len(self.name) > 1 else ""
 
     def flatten(self, keep_base_note_name: bool):
+        """
+        Does not change base note name if not requested, just adds flat.
+        Sometimes it is more proper to have a Bbb rather than A. 
+        """
         if keep_base_note_name:
             self.alter += FLAT
             self.name += FLAT
@@ -58,7 +65,8 @@ class NoteGeneric:
 class Note(NoteGeneric):
 
     """
-    To allow for more significant alterations, the Note class.
+    To allow for more significant use, the Note class.
+    Holds pitch, velocity, duration, start time, midi value for the note.
     """
 
     def __init__(self, midi_value: int, duration:int, velocity:int, start_time:any, *args, **kwargs) -> None:
@@ -67,6 +75,7 @@ class Note(NoteGeneric):
         self.duration = duration
         self.velocity = velocity
         self.start_time = start_time
+        assert midi_start_val < self.midi_value < midi_end_val , f"MIDI value is out of range for {self.name} with value of {self.midi_value}"
         self.pitch = get_pitch_from_midi_value(self.midi_value)
 
     def __eq__(self, __o: object) -> bool:
@@ -76,17 +85,21 @@ class Note(NoteGeneric):
             return False
 
     def change_name_notation(self, new_base_note: str, new_alter: str) -> None:
+        """
+        Sometimes it is more proper to have a Bbb rather than A. 
+        If the name needs to be changed to A though this fn does so        
+        """
         self.base_note_name = new_base_note
         self.alter = new_alter
         self.name = self.base_note_name + self.alter
 
     def sharpen(self, keep_base_note_name: bool = True):
-        """Does not change base note name if not requested, just adds sharp"""
+        """Does not change base note name if not requested, just adds sharp to the str"""
         super().sharpen(keep_base_note_name)
         self.pitch = self.pitch * np.power(2, 1 / 12)
 
     def flatten(self, keep_base_note_name: bool = False):
-        """Does not change base note name if not requested, just adds flat"""
+        """Does not change base note name if not requested, just adds flat to the str"""
         super().flatten(keep_base_note_name)
         self.pitch = self.pitch * np.power(2, -1 * (1 / 12))
 
